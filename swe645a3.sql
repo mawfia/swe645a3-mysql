800f66ebc5df48bc7dbe0db04d858a903ba4c3cb
Andrew <michael.a.williams@mawfia.com>
2020-11-03 10:21:30 -0500

    Added zip code field

---
+++ swe645a3.sql
@@ -0,0 +1,66 @@
+CREATE DATABASE  IF NOT EXISTS `swe645a3` /*!40100 DEFAULT CHARACTER SET utf8 COLLATE utf8_bin */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `swe645a3`;
+-- MySQL dump 10.13  Distrib 8.0.19, for Win64 (x86_64)
+--
+-- Host: localhost    Database: swe645a3
+-- ------------------------------------------------------
+-- Server version	8.0.19
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `survey`
+--
+
+DROP TABLE IF EXISTS `survey`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `survey` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `first_name` varchar(25) COLLATE utf8_bin NOT NULL,
+  `last_name` varchar(25) COLLATE utf8_bin NOT NULL,
+  `address` varchar(25) COLLATE utf8_bin NOT NULL,
+  `city` varchar(25) COLLATE utf8_bin NOT NULL,
+  `state` varchar(21) COLLATE utf8_bin NOT NULL,
+  `zip` varchar(5) COLLATE utf8_bin NOT NULL,
+  `telephone` varchar(10) COLLATE utf8_bin NOT NULL,
+  `email` varchar(25) COLLATE utf8_bin NOT NULL,
+  `date_of_survey` date NOT NULL,
+  `like_most` int NOT NULL,
+  `referral` int NOT NULL,
+  `recommend` int NOT NULL,
+  `comments` varchar(45) COLLATE utf8_bin DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `id_UNIQUE` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `survey`
+--
+
+LOCK TABLES `survey` WRITE;
+/*!40000 ALTER TABLE `survey` DISABLE KEYS */;
+INSERT INTO `survey` VALUES (1,'michael','williams','2129 pam court','irving','tx','78712','8475255417','mawfia@gmail.com','2020-09-03',0,1,1,'0'),(3,'mila','johnson','1008 n glebe rd','arlington','va','22134','7189167425','mawfia@yahoo.com','2020-10-23',1,2,2,'Testing create from scratch.');
+/*!40000 ALTER TABLE `survey` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2020-11-03 10:16:37
